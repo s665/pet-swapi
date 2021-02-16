2e8590d2e8d9e1de63550824fa5905959d687160
Stepan Skaryakin <s6658895@gmail.com>
2021-02-16 14:05:49 +0500

    initial project and add dependencies

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const App = () => {
+    return (
+        <div className={style.app}>
+            App
+        </div>
+    );
+}
+
+export default App;
