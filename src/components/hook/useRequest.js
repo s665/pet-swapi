267a5c807bb6f1efb3f72c00701373f580d5f46b
Stepan Skaryakin <s6658895@gmail.com>
2021-02-19 21:03:20 +0500

    add hook useRequest

--- src/components/hook/useRequest.js
+++ src/components/hook/useRequest.js
@@ -1,7 +1,29 @@
+import {useEffect, useState} from "react";
 
 const useRequest = (request) => {
 
+    const [dataState, setDataState] = useState({
+        loading: true,
+        error: false,
+        data: null
+    })
 
+    useEffect(() => {
+        const canceled = false
+        request()
+            .then(data => !canceled && setDataState({
+                loading: false,
+                error: false,
+                data
+            }))
+            .catch(error => !canceled && setDataState({
+                loading: false,
+                error,
+                data: null
+            }))
+    }, [request])
+
+    return dataState
 
 }
 
